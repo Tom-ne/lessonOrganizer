8153b08af869bd593cd8c6fadda5807c209861cf
Tom Neumann <tomn505@gmail.com>
2022-11-15 16:02:17 +0200

    added option to open the lesson folder in explorer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import tkinter as tk
+import tkinter.messagebox as messagebox
+import subprocess
 
 # Creating the window
 root = tk.Tk()
@@ -20,6 +22,11 @@ def create():
             os.mkdir(path)
         except OSError:
             pass
+
+    # Making a function to open the directory in file explorer
+    def openExplorer(path):
+        FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
+        subprocess.run([FILEBROWSER_PATH, lessonPath])
 
     # Getting entries from the inputs
     yearField = year.get()
@@ -46,6 +53,16 @@ def create():
     # Creating all directories
     for path in paths:
         createDirectory(path)
+
+    # Asking the user if he wants to open the folder in file explorer
+    openInExplorer = messagebox.askquestion('Open Explorer', 'Do you want to open the lesson directory in file explorer?')
+
+    if openInExplorer == 'yes':
+        # Opening the directory in file explorer
+        openExplorer(lessonPath)
+
+        # Closing the program
+        root.destroy()
     
 # Adding a label and entry for year, cemester, course and lesson and adding them to the screen
 yearText = tk.Label(root, text="Enter the current learning year" ,width=150).pack()
